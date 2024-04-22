630f7b34f1111a169ee751d01b341f087366f4c8
Tanjilur Rahman Jim <tanjilurrahman21@gmail.com>
2024-04-23 05:28:36 +0600

    deployment to vercel
    changed in settings.py -> allowed hosts
    wsgi.py app = application

--- library/settings.py
+++ library/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-s%nm7hz^$k8w1!u834v_&++nnk5#oso7)#3*bc7_0__eaqtwqa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
